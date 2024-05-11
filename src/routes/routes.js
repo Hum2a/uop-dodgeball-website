4a7180ee08d807b572b131d67982dca7f161146b
Humza Butt <72743279+UP942033@users.noreply.github.com>
2024-05-11 16:41:37 +0100

    Added Committee and History Links

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,8 @@
 // src/routes/routes.js
 import Home from '../pages/Home/Home';
 import FamilyTree from '../pages/Family Tree/FamilyTree';
+import Committee from '../pages/Committee/Committee';
+import History from '../pages/History/History';
 
 const routes = [
   {
@@ -11,6 +13,14 @@ const routes = [
   {
     path: '/family-tree',
     element: <FamilyTree />,
+  },
+  {
+    path: '/committee',
+    element: <Committee />,
+  },
+  {
+    path: '/history',
+    element: <History />,
   }
 ];
 
